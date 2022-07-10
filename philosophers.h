cdb407d0fe37972626830c7be2eac9d315776541
Chaimaa Bourajli <cbourajl@student.1337.ma>
2022-07-10 16:30:54 +0100

    hellolemonde

---
+++ philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbourajl <cbourajl@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 15:53:37 by cbourajl          #+#    #+#             */
+/*   Updated: 2022/07/10 15:53:37 by cbourajl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <pthread.h>
+
+#endif
