2822dabab9efa402e3bde79f6bacf691c070d0af
Chaimaa Bourajli <cbourajl@e3r6p6.1337.ma>
2022-07-23 14:19:55 +0100

    another useless project

--- time.c
+++ time.c
@@ -6,35 +6,35 @@
 /*   By: cbourajl <cbourajl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 09:58:06 by cbourajl          #+#    #+#             */
-/*   Updated: 2022/07/23 12:23:16 by cbourajl         ###   ########.fr       */
+/*   Updated: 2022/07/23 13:47:43 by cbourajl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long long   time_passed(long long debut)
+long long	time_passed(long long debut)
 {
-    long long   ecoule;
-    struct timeval  time;
+	long long		ecoule;
+	struct timeval	time;
 
-    gettimeofday(&time, NULL);
-    ecoule = (time.tv_sec * 1000) + (time.tv_usec / 1000) - debut;
-    return (ecoule);
+	gettimeofday(&time, NULL);
+	ecoule = (time.tv_sec * 1000) + (time.tv_usec / 1000) - debut;
+	return (ecoule);
 }
 
-long long   current_time(void)
+long long	current_time(void)
 {
-    struct timeval  time;
+	struct timeval	time;
 
-    gettimeofday(&time, NULL);
-    return ((time.tv_sec * 1000) + time.tv_usec / 1000);
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + time.tv_usec / 1000);
 }
 
-void    ft_sleep(int time)
+void	ft_sleep(int time)
 {
-    long long   t;
+	long long	t;
 
-    t = current_time();
-    while (current_time() - t < time)
-        usleep(100);
+	t = current_time();
+	while (current_time() - t < time)
+		usleep(100);
 }
